bf9a7c6cbd9b033d794265cc1124da4442a02b5e
Vibol T <vibolteav@gmail.com>
2021-03-04 16:52:03 +0700

    added whois.nic.domains

---
+++ lib/whois/parsers/whois.nic.domains.rb
@@ -0,0 +1,24 @@
+#--
+# Ruby Whois
+#
+# An intelligent pure Ruby WHOIS client and parser.
+#
+# Copyright (c) 2009-2018 Simone Carletti <weppos@weppos.net>
+#++
+
+
+require_relative 'base_whoisd'
+
+
+module Whois
+  class Parsers
+
+    # Parser for the whois.nic.domains server.
+    #
+    # @see Whois::Parsers::Example
+    #   The Example parser for the list of all available methods.
+    #
+    class WhoisNicDomains < BaseVerisign
+    end
+  end
+end
